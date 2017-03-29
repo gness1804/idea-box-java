c8754d63a2c0bbe8cc083ca7b825fe889eee1830
gness1804 <graham.nessler@gmail.com>
2017-03-29 17:06:20 -0600

    Create name input field

--- app/src/main/java/com/example/grahamnessler/ideabox/MainActivity.java
+++ app/src/main/java/com/example/grahamnessler/ideabox/MainActivity.java
@@ -10,12 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        createIdea();
     }
 
     void createIdea() {
         Idea idea = new Idea("Hello", "I am the idea", "Swill");
-        String name = idea.getName();
-        Log.d("The name", name);
+
     }
 }
