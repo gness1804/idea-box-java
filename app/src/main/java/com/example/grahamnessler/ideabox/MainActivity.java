48ce889ca00c7dea2fa033867ae418b9b058a5d9
gness1804 <graham.nessler@gmail.com>
2017-03-29 19:20:19 -0600

    Dropdown recognizes item selected

--- app/src/main/java/com/example/grahamnessler/ideabox/MainActivity.java
+++ app/src/main/java/com/example/grahamnessler/ideabox/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.grahamnessler.ideabox;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +18,28 @@ public class MainActivity extends AppCompatActivity {
         createSpinner();
     }
 
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        switch (pos) {
+            case 0:
+                Log.d("Message", "First item selected");
+                break;
+            default:
+                Log.d("Message", "Nothing valid selected");
+        }
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
     void createSpinner () {
-        Spinner spinner = (Spinner) findViewById(R.id.ideaQualityInput);
-// Create an ArrayAdapter using the string array and a default spinner layout
+        spinner = (Spinner) findViewById(R.id.ideaQualityInput);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.idea_quality_list, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
     void createIdea() {
