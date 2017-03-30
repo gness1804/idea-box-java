2191400cacb82578cad8e0cb76efa376b785ecfd
gness1804 <graham.nessler@gmail.com>
2017-03-29 18:11:48 -0600

    Add dropdown

--- app/src/main/java/com/example/grahamnessler/ideabox/MainActivity.java
+++ app/src/main/java/com/example/grahamnessler/ideabox/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.grahamnessler.ideabox;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createSpinner();
+    }
+
+    void createSpinner () {
+        Spinner spinner = (Spinner) findViewById(R.id.ideaQualityInput);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.idea_quality_list, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     void createIdea() {
